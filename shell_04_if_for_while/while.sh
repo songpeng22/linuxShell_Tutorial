37f2dcbd50bad034cc366c5a375091aea3873f4d
peng <songpeng24@msn.com>
2020-08-30 16:53:04 +0800

    read file line by line

--- shell_04_if_for_while/while.sh
+++ shell_04_if_for_while/while.sh
@@ -8,6 +8,7 @@ do
 	echo $number
 	number=$(( number + 1 ))
 done
+echo -e "\n"
 
 # until loop
 echo "until loop"
@@ -17,3 +18,22 @@ do
 	echo $number
 	number=$(( number + 1 ))
 done
+echo -e "\n"
+
+# while loop , read file line by line
+echo "while loop , read file line by line"
+inputfile=./textfile.txt
+while read -r line
+do
+    echo $line
+done < "$inputfile"
+echo -e "\n"
+
+# while loop , read lines from command
+echo "while loop , read lines from command"
+while IFS= read -r line
+do
+   ## take some action on $line
+  echo "$line"
+done < <(ps u)
+echo -e "\n"
